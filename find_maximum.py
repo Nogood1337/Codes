b5264ae78fc1c461e7fbb2614cc9503ab847350f
Nogood1337 <nogood_1337@hotmail.com>
2017-06-29 11:01:25 +0200

    Newer versions
    Including execution time counter

--- find_maximum.py
+++ find_maximum.py
@@ -2,15 +2,22 @@
 # -*- coding:utf-8 -*-
 #standard python packages
 import numpy as np
-import math
+import math, time
 
 ##################################################
 #changeable things
 
-outname = "text_files/" +  "maximum.txt"	#in case for saving
-namelist = ["BlueJet1_128GTU.txt", "BlueJet2_128GTU.txt", "BlueJet3_128GTU.txt", "BlueJet5_128GTU.txt", "BlueJet6_128GTU.txt", "BlueJet7_128GTU.txt", "Elves1_128GTU.txt", "Elves2_128GTU.txt", "Sprite1_128GTU.txt", "Sprite2_128GTU.txt", "Meteor_1_L2.txt", "Meteor_2_L2.txt"]	#in case of wanting to go through multiple files
+#outname = "text_files/" +  "maximum.txt"	#in case for saving
+#namelist = ["BlueJet1_128GTU.txt", "BlueJet2_128GTU.txt", "BlueJet3_128GTU.txt", "BlueJet5_128GTU.txt", "BlueJet6_128GTU.txt", "BlueJet7_128GTU.txt", "Elves1_128GTU.txt", "Elves2_128GTU.txt", "Sprite1_128GTU.txt", "Sprite2_128GTU.txt", "Meteor_1_L2.txt", "Meteor_2_L2.txt"]	#in case of wanting to go through multiple files
+want_time = 0	#0 for wanting to count execution time and print it in the terminal, anything else will not
+
+#below is for testing
+outname = "text_files/" +  "test_max.txt"
+namelist = ["Meteor_1_L2.txt", "Meteor_2_L2.txt"]
 
 ##################################################
+
+start = time.time()
 
 maximimum_collection = []
 for name in namelist:
@@ -40,4 +47,8 @@ for name in namelist:
 thefile = open(outname, 'w')
 for line in maximimum_collection:	#goes through the total_list and writes each line in the text file
 	thefile.write(line + "\n")
+
+end = time.time()
+if want_time == 0:
+	print end-start
 
